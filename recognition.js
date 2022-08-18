ced5107c8cd6e5382f27a98f9011b77fee8bb902
Ulises Mariano Melgarejo <73831325+Nemo3003@users.noreply.github.com>
2022-08-18 18:44:52 -0300

    Create Recognition

---
+++ recognition.js
@@ -0,0 +1,11 @@
+//This is intended to be executed in the browser
+
+const recognition = new webkitSpeechRecognition()
+recognition.lang = 'es-ES';
+recognition.continuous = true;
+recognition.onresult = event =>{
+  for(const result of event.results){
+    console.log(result[0].transcript)
+  }
+}
+recognition.start()
